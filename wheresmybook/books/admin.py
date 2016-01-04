5e8e44ec80b982ac34680937605b004afeebe79d
Celine Boudier <boudier.celine@gmail.com>
2016-01-04 11:00:43 +0000

    Improved models and admin

--- wheresmybook/books/admin.py
+++ wheresmybook/books/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Employee, Shelf, Book
+
+admin.site.register(Employee)
+admin.site.register(Shelf)
+admin.site.register(Book)
